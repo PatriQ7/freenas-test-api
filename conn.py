9a12b97bd3de16f14e3ee268fe8f4aec95ed590b
PatriQ7 <qiqipat@gmail.com>
2014-06-09 17:15:00 -0700

    REST Connection Info

---
+++ conn.py
@@ -0,0 +1,11 @@
+import requests
+
+
+user_name = raw_input('Username: ')
+user_passwd = raw_input('Password: ')
+url = 'http://freenas-test1.sjlab1.ixsystems.com/api/v1.0/'
+auth = (user_name,user_passwd)
+#  def __init__(self,url,auth):
+#    self.url = 'http://freenas-test1.sjlab1.ixsystems.com/api/v1.0/'
+#    self.auth = ('root','patrick')
+patyment = 3564.21
